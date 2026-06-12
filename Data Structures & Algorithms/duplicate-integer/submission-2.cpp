19e0b44393eba7f02a1aaae2bcb4f44baf6ae62b
Huy Thiệp <285228587+huythiep-coder@users.noreply.github.com>
2026-06-12 13:30:13 +0700

    Update: duplicate-integer - submission-2

--- Data Structures & Algorithms/duplicate-integer/submission-2.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -1,24 +1,14 @@
-// Thuật toán Hash Set(0(n))
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        /* for-each :
-         for (int i = 0 ; i < nums.size() ; i ++)
+        unordered_set <int> seen;
+        for(int i = 0 ; i < nums.size() ; i ++)
         {
-            int x = nums[i];
-        }
-        */
-
-        for(int x : nums)
-        {
-            // nếu tìm thấy giá trị x tồn tại trong mảng
-            if(seen.count(x))
+            if(seen.count(nums[i]))
             {
                 return true;
             }
-            // nếu ko tìm thấy thì lưu vào
-            seen.insert(x);
+            seen.insert(nums[i]);
         }
         return false;
     }
